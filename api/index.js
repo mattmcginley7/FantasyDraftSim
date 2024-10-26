f958669b4998fbae716e73f27d285aea2fb5ea17
Matt McGinley <matthewmcginley@live.com>
2024-10-25 20:23:39 -0400

    added cors functionality

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
+app.use(cors());
+
 
 const app = express();
 const port = process.env.PORT || 3000;
